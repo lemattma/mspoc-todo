7ae790340fe50693457d9a1b7283e325e4822d7f
Martin Miranda <martinmirandac@gmail.com>
2020-01-24 23:29:23 +1100

    todo model

---
+++ db/migrate/20200124122718_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :todos do |t|
+      t.string :description
+      t.boolean :completed
+
+      t.timestamps
+    end
+    add_index :todos, :completed
+  end
+end
